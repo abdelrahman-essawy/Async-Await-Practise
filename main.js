d9ccab55f2767eb09ee30794ed0afd8a4b481b7a
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-09-30 14:36:26 +0200

    applying async await, making code more readable.

--- main.js
+++ main.js
@@ -1,46 +1,22 @@
 'strict mode'
 
-let getMoviesFromApi = (() => {
-    return new Promise((resolved, rejected) => {
-        let myHttp = new XMLHttpRequest();
-        myHttp.open('GET', 'https://api.themoviedb.org/3/trending/all/week?api_key=384d8dfa8859f8bf3f596c1cf4a030a2', true)
-        myHttp.send()
-        myHttp.addEventListener('load', () => {
-            if (myHttp.readyState == 4 && myHttp.status == 200) {
-                let moviesList = JSON.parse(myHttp.response).results
-                resolved(moviesList);
-            } else {
-                rejected();
-            }
-        })
-    })
-})
+let getMoviesFromApi = async () => {
+    let respone = await fetch('https://api.themoviedb.org/3/trending/all/week?api_key=384d8dfa8859f8bf3f596c1cf4a030a2').catch('Error in Connection')
+    let moviesList = (await respone.json())['results']
+    displayMovies(moviesList)
+}
 
 let displayMovies = (moviesList) => {
     let content = ``
-    for (let i = 0; i < moviesList.length; i++) {
-        const movie = moviesList[i];
-        if (movie.name) {
-            content += `
+    moviesList.forEach(movie => {
+        content += `
             <div class="col-md-4 my-5">
             <img class="w-100 rounded" src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}">
-            <h3>${movie.name}</h3>
+            <h3>${Object.values(movie)[3]}</h3>
             </div>
             `
-        }
-        if (movie.title) {
-            content += `
-            <div class="col-md-4 my-5">
-            <img class="w-100 rounded" src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}">
-            <h3>${movie.title}</h3>
-            </div>
-            `
-        }
-
-    }
+    });
     document.getElementById('moviesListId').innerHTML = content
-
 }
-getMoviesFromApi().then(displayMovies)
-    .catch(() => console.log("Connection Error"));
 
+(() => getMoviesFromApi())()
